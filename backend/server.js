32df9754d76e32f475e49e5f03ca7c64ce16269a
Aayush Kumar Shrivastava <aayushs894@gmail.com>
2024-05-07 01:14:04 +0530

    Final release

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, io, server } from "./socket/socket.js";
 
+const __dirname = path.resolve();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
@@ -20,6 +21,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*",(req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
