f86caf1fe4cc9ce0a33037e3beee935dfd7fb2de
Aayush Kumar Shrivastava <aayushs894@gmail.com>
2024-05-10 16:21:43 +0530

    Add rate limiter

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -2,13 +2,26 @@ import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 import cors from 'cors'; 
+import { rateLimit } from "express-rate-limit";
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+});
 
 const app = express();
-app.use(cors({
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Credentials': true,
-}
-));
+
+app.use(
+  cors({
+    "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Credentials": true,
+  })
+);
+
+app.use(limiter) ; 
 
 const server = http.createServer(app);
 const io = new Server(server, {
